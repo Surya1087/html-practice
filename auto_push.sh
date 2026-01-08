84b6151ede82131db6a36ffc2a6241688712ee66
Surya Pratap Singh <suryaujjain8@gmail.com>
2026-01-08 22:20:56 +0530

    Auto commit

---
+++ auto_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Go to your repo folder
+cd /Users/suryapratapsingh/Desktop/DEVELOPMENT
+
+# Add all changes
+/usr/bin/git add .
+
+# Commit with message
+/usr/bin/git commit -m "Auto commit"
+
+# Push to GitHub
+/usr/bin/git push origin main
+
